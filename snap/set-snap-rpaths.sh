2d5f688f5220e941e443a19dd93f0e2566e3cad5
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-05-25 22:08:19 +0300

    snap: set binaries rpath to load runtime libraries
    So we can avoid to set LD_PRELOAD and LD_LIBRARY_PATH in the wrapper and
    eventually to drop them from the terminal too

---
+++ snap/set-snap-rpaths.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+if [ -z "$SNAPCRAFT_PROJECT_NAME" ]; then
+  echo "This script has to be called from snapcraft"
+  exit 1
+fi
+
+if [ -z "$SNAPCRAFT_RPATH_RUNTIMES" ]; then
+  echo 'The varialbe $SNAPCRAFT_RPATH_RUNTIMES is not defined'
+  exit 1
+fi
+
+for cnt in $SNAPCRAFT_PROJECT_NAME $SNAPCRAFT_RPATH_RUNTIMES core; do
+  rpath+="$([ -n "$rpath" ] && echo "$rpath:" || echo "")"
+  rpath+="/snap/$cnt/current/lib"
+  rpath+=":/snap/$cnt/current/lib/$SNAPCRAFT_ARCH_TRIPLET"
+  rpath+=":/snap/$cnt/current/usr/lib"
+  rpath+=":/snap/$cnt/current/usr/lib/$SNAPCRAFT_ARCH_TRIPLET"
+done
+
+patchelf --force-rpath --set-rpath "$rpath" $@
